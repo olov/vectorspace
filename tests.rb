8a84477fa52af05774abb7fce8025d2749bf4aa4
Olov Lassus <olov.lassus@gmail.com>
2008-11-08 21:28:32 +0100

    tidied tests

--- tests.rb
+++ tests.rb
@@ -47,18 +47,18 @@ class TestVec < Test::Unit::TestCase
   end
   def test_get_attributes
     v = Vec[1, 2.3, 3]
-    assert_equal(v.x, 1)
-    assert_equal(v.y, 2.3)
-    assert_equal(v.z, 3.0)
+    assert_equal(1, v.x)
+    assert_equal(2.3, v.y)
+    assert_equal(3.0, v.z)
   end
   def test_set_attributes
     v = Vec[1, 2.3, 3]
     v.x = 2
     v.y = 3
     v.z = 4.3
-    assert_equal(v.x, 2)
-    assert_equal(v.y, 3.0)
-    assert_equal(v.z, 4.3)
+    assert_equal(2, v.x)
+    assert_equal(3.0, v.y)
+    assert_equal(4.3, v.z)
   end
   def test_get_length
     assert_equal(1, Vec[1, 0, 0].length)
@@ -71,15 +71,15 @@ class TestVec < Test::Unit::TestCase
     v2 = Vec[1, 2, 3]
     v2.length = 2
     assert_equal(v2.length, 2)
-    assert_equal(v2.x / v1.x, v2.length / v1.length)
-    assert_equal(v2.y / v1.y, v2.length / v1.length)
-    assert_equal(v2.z / v1.z, v2.length / v1.length)
+    assert_equal(v2.length / v1.length, v2.x / v1.x)
+    assert_equal(v2.length / v1.length, v2.y / v1.y)
+    assert_equal(v2.length / v1.length, v2.z / v1.z)
   end
   def test_scale_up
     v1 = Vec[1, 2, 3]
     v2 = Vec[2, 4, 6]
     v1.length *= 2
-    assert_equal(v1, v2)
+    assert_equal(v2, v1)
   end
   def test_scale_down
     v1 = Vec[1, 2, 3]
@@ -90,41 +90,41 @@ class TestVec < Test::Unit::TestCase
   def test_normalize
     v1 = Vec[1, 2, 3]
     v2 = Vec[1, 2, 3].normalize!
-    assert_equal(v2.length, 1)
-    assert_equal(v1.length * v2.x, v1.x)
-    assert_equal(v1.length * v2.y, v1.y)
-    assert_equal(v1.length * v2.z, v1.z)
+    assert_equal(1, v2.length)
+    assert_equal(v1.x, v1.length * v2.x)
+    assert_equal(v1.y, v1.length * v2.y)
+    assert_equal(v1.z, v1.length * v2.z)
   end
   def test_add
     v1 = Vec[1, 2, 3]
     v2 = Vec[4, 5, 6]
-    assert_equal(v1 + v2, Vec[5, 7, 9])
+    assert_equal(Vec[5, 7, 9], v1 + v2)
     assert_equal(v1 + v2, v1.add(v2))
-    assert_equal(v1 + 2, Vec[3, 4, 5])
+    assert_equal(Vec[3, 4, 5], v1 + 2)
     assert_equal(v1 + 2, v1.add(2))
   end
   def test_sub
     v1 = Vec[4, 6, 8]
     v2 = Vec[1, 2, 3]
-    assert_equal(v1 - v2, Vec[3, 4, 5])
+    assert_equal(Vec[3, 4, 5], v1 - v2)
     assert_equal(v1 - v2, v1.sub(v2))
-    assert_equal(v1 - 2, Vec[2, 4, 6])
+    assert_equal(Vec[2, 4, 6], v1 - 2)
     assert_equal(v1 - 2, v1.sub(2))
   end
   def test_mul
     v1 = Vec[4, 6, 8]
     v2 = Vec[1, 2, 3]
-    assert_equal(v1 * v2, Vec[4, 12, 24])
+    assert_equal(Vec[4, 12, 24], v1 * v2)
     assert_equal(v1 * v2, v1.mul(v2))
-    assert_equal(v1 * 3, Vec[12, 18, 24])
+    assert_equal(Vec[12, 18, 24], v1 * 3)
     assert_equal(v1 * 3, v1.mul(3))
   end
   def test_div
     v1 = Vec[4, 6, 15]
     v2 = Vec[1, 2, 3]
-    assert_equal(v1 / v2, Vec[4, 3, 5])
+    assert_equal(Vec[4, 3, 5], v1 / v2)
     assert_equal(v1 / v2, v1.div(v2))
-    assert_equal(v1 / 3.0, Vec[4/3.0, 2, 5])
+    assert_equal(Vec[4/3.0, 2, 5], v1 / 3.0)
     assert_equal(v1 / 3, v1.div(3))
   end
   def test_dot_angle
@@ -143,10 +143,10 @@ class TestVec < Test::Unit::TestCase
     assert_equal(v1 % v2, v1.cross(v2))
   end
   def test_coerce
-    v1 = Vec[-1, 1, 3]
-    assert_equal(Vec[1, 3, 5], 2 + v1)
-    assert_equal(Vec[3, 1, -1], 2 - v1)
-    assert_equal(Vec[-2, 2, 2.0/3], 2.0 / v1)
-    assert_equal(Vec[-2, 2, 6], 2 * v1)
+    v = Vec[-1, 1, 3]
+    assert_equal(Vec[1, 3, 5], 2 + v)
+    assert_equal(Vec[3, 1, -1], 2 - v)
+    assert_equal(Vec[-2, 2, 2.0/3], 2.0 / v)
+    assert_equal(Vec[-2, 2, 6], 2 * v)
   end
 end
